d03ed8fd52b17fec75014701a795c75dc0e722e6
Tom Lovett <tomlovett6@gmail.com>
2018-05-23 19:19:16 -0400

    Fix route namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  scope 'api' do
-    resource :games, only: [:create, :index, :show, :update]
-    resource :players, only: [:create, :index, :show, :update, :destroy]
-    resource :points, only: [:create, :index, :update]
-    resource :stats, only: [:create]
-    resource :teams, only: [:create, :index, :show, :update]
-    resource :users, only: [:create, :show, :update]
-  end
+  resources :games, only: [:create, :index, :show, :update]
+  resources :players, only: [:create, :index, :show, :update, :destroy]
+  resources :points, only: [:create, :index, :update]
+  resources :stats, only: [:create]
+  resources :teams, only: [:create, :index, :show, :update]
+  resources :users, only: [:create, :show, :update]
 end
